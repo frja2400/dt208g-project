d24f0b2c0e74d79cd068bce81d2f53bdf2d60333
Frida Jansson <frja2400@student.miun.se>
2025-05-15 13:43:52 +0200

    Importerar FormsModule. Utvecklar en filterfunktion i min CourseComponent. Använder tvåvägs-databindning med ngModel och använder en if-sats i min html-komponent för att skriva ut meddelande om inga kurser hittas på filtreringen.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common'; //Importerar CommonModule för n
 import { Component } from '@angular/core';
 import { Course } from '../models/course';      //Interface av kurser
 import { CourseService } from '../services/course.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
   //Properties
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  filterValue: string = "";
 
   //Skapar instans av CourseService
   constructor(private courseService: CourseService) {}
@@ -20,6 +23,16 @@ export class CoursesComponent {
   ngOnInit() {
     this.courseService.getCourses().subscribe((courses) => {
       this.courses = courses;
+      this.filteredCourses = courses;
     })
   }
+
+  applyFilter(): void {
+    this.filteredCourses = this.courses.filter((course) =>
+      //Filtrera på antingen kurskod eller kursnamn.
+      course.courseCode.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      course.courseName.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
+
 }

5fa78f9e4c53ffebcfee16f95c7e12004abdd628
Frida Jansson <frja2400@student.miun.se>
2025-05-15 13:04:04 +0200

    Skapade ett interface för min kurslista. Importerade HttpClient. Använde Observables och HttpClient i min CourseService för att hämta data från min json-fil.

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,2 +1,8 @@
+//Inerface för data jag hämtar från kurser
 export interface Course {
+    courseCode: string,
+    courseName: string,
+    points: number,
+    subject: string,
+    syllabus: string
 }
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Course } from '../models/course';  //Importerar mitt interface
+import { Observable } from 'rxjs';          //Importerar observables
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  //Properties
+  private courseURL = '/miun_courses.json';
+
+  constructor(private http: HttpClient) { }
+
+  //Returnerar en observable (liknar promise) med en lista av objekt enligt mitt interface.
+  getCourses(): Observable<Course[]> {
+    //Skickar ett get-anrop till min json-fil.
+    return this.http.get<Course[]>(this.courseURL);
+  }
 }

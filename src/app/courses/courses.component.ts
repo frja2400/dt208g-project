3cfb768c6e76a7847e0dee967cfa0ed3a0f815c0
Frida Jansson <frja2400@student.miun.se>
2025-05-15 13:23:30 +0200

    Utvecklade min CoursesComponent och hämtade data från JSON-filen med ngOnInit och HttpClient. Adderade ngFor i min html-kod för att lista kurserna i en tabell.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common'; //Importerar CommonModule för ngFor
 import { Component } from '@angular/core';
+import { Course } from '../models/course';      //Interface av kurser
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+  //Properties
+  courses: Course[] = [];
 
+  //Skapar instans av CourseService
+  constructor(private courseService: CourseService) {}
+
+  //Anropar metoden i CourseService
+  ngOnInit() {
+    this.courseService.getCourses().subscribe((courses) => {
+      this.courses = courses;
+    })
+  }
 }

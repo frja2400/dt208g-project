72e1bf69a38c0681124cb6df7594cd6dee62f2dd
Frida Jansson <frja2400@student.miun.se>
2025-05-16 16:11:10 +0200

    Implementerade Angular Material. Ändrade min manuella kurslista och använde MatTable istället. Ändrade sortering och filtrering med matSort, matInput, matSelect. Adderade paginering medmat-paginator.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,120 +1,95 @@
 import { CommonModule } from '@angular/common';                       //Importerar CommonModule för ngFor.
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Course } from '../models/course';                            //Interface av kurser.
 import { CourseService } from '../services/course.service';
 import { FormsModule } from '@angular/forms';
 import { ScheduleService } from '../services/schedule.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-courses',
-  imports: [CommonModule, FormsModule],
+  //Importerar Agular Material-moduler
+  imports: [CommonModule, FormsModule, MatTableModule, MatSortModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
   //Properties
-  courses: Course[] = [];
-  filteredCourses: Course[] = [];
+  displayedColumns: string[] = ['courseCode', 'courseName', 'points', 'subject', 'syllabus', 'add'];    //Tabeller som ska visas i Angular Material-tabellen
+  dataSource = new MatTableDataSource<Course>();                                                        //Hanterar sortering, filtrering och paginering
+  subjects: string[] = [];                                                                              //Lista över ämnen som hämtas till drop down-menyn
   filterValue: string = "";
-  subjects: string[] = [];
   selectedSubject: string = "";
 
-  //Ger min komponent tillgång till mina services.
+  //Deklarerar referenser till sortering och paginering som kopplas med <mat-sort> och <mat-paginator> i HTML.
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  //Injicerar mina två services.
   constructor(
     private courseService: CourseService,
     private scheduleService: ScheduleService
   ) { }
 
-  //Anropar metoden i CourseService
   ngOnInit() {
+    //Hämtar kurser från CourseService och lägg till i dataSource.
     this.courseService.getCourses().subscribe((courses) => {
-      this.courses = courses;
-      this.filteredCourses = courses;
+      this.dataSource.data = courses;     
 
-      //Hämta alla ämnen från datan
-      const subjects = this.courses.map(course => course.subject);
-      this.subjects = [...new Set(subjects)];   //Undviker dubletter
-    })
-  }
+      //Mappa alla ämnen, addera i subjects och använder Set för att ta bort dubletter.
+      const subjects = courses.map(course => course.subject);
+      this.subjects = [...new Set(subjects)];
 
-  applyFilters(): void {
-    this.filteredCourses = this.courses.filter((course) => {
-      //Filtrera på kurskod eller kursnamn i sökrutan.
-      const filterByText =
-        course.courseCode.toLowerCase().includes(this.filterValue.toLowerCase()) ||
-        course.courseName.toLowerCase().includes(this.filterValue.toLowerCase());
+      //Koppla AM-paginator och sortering till dataSource.
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
 
-      //Filtrera på valt ämne
-      const filterBySubject = this.selectedSubject === "" || course.subject === this.selectedSubject;
+      //Anpassad filterfunktion som kombinerar text + ämne
+      this.dataSource.filterPredicate = (data: Course, filter: string) => {
+        const searchObj = JSON.parse(filter);
+        const searchText = searchObj.text;
+        const selectedSubject = searchObj.subject;
 
-      return filterByText && filterBySubject
+        const matchText =
+          data.courseCode.toLowerCase().includes(searchText) ||
+          data.courseName.toLowerCase().includes(searchText);
+
+        const matchSubject = selectedSubject === '' || data.subject === selectedSubject;
+
+        return matchText && matchSubject;
+      };
     });
   }
 
-  //Sorteringsmetoder
-  //Sorteringsriktning, true = stigande. Används för att växla mellan stigande och fallande.
-  sortDirection = {
-    courseCode: true,
-    courseName: true,
-    points: true,
-    subject: true
-  }
-
-  sortCode(): void {
-    const direction = this.sortDirection.courseCode;
-    this.filteredCourses.sort((a, b) =>
-      //Använder localCompare för att jämföra strängar och ta hänsyn till språk.
-      //En ternär operator väljer stigande eller fallande beroende på om direction är true eller false.
-      direction ? a.courseCode.localeCompare(b.courseCode) : b.courseCode.localeCompare(a.courseCode)
-    );
-    //Byter riktning för nästa gång man klickar.
-    this.sortDirection.courseCode = !direction;
-  }
-
-  sortName(): void {
-    const direction = this.sortDirection.courseName;
-    this.filteredCourses.sort((a, b) =>
-      direction ? a.courseName.localeCompare(b.courseName) : b.courseName.localeCompare(a.courseName)
-    );
-    this.sortDirection.courseName = !direction;
-  }
-
-  sortPoints(): void {
-    const direction = this.sortDirection.points;
-    this.filteredCourses.sort((a, b) =>
-      direction ? a.points - b.points : b.points - a.points
-    );
-    this.sortDirection.points = !direction;
-  }
-
-  sortSubject(): void {
-    const direction = this.sortDirection.subject;
-    this.filteredCourses.sort((a, b) =>
-      direction ? a.subject.localeCompare(b.subject) : b.subject.localeCompare(a.subject)
-    );
-    this.sortDirection.subject = !direction;
-  }
-
-  //Metod som rensar val, filtrering, sortering
-  clearChoices(): void {
-    this.filterValue = "";
-    this.selectedSubject = "";
-
-    this.sortDirection = {
-      courseCode: true,
-      courseName: true,
-      points: true,
-      subject: true
+  applyFilters(): void {
+    //Skapar filterobjekt baserat på sökord och valt ämne.
+    const filterObj = {
+      text: this.filterValue.trim().toLowerCase(),
+      subject: this.selectedSubject
     };
 
-    //Kopierar courses och gör en ny array. För att undvika att ändringar i filteredCourses påverkar courses. 
-    this.filteredCourses = [...this.courses];
+    this.dataSource.filter = JSON.stringify(filterObj);
+
+    //När filtrering görs - hoppa tillbaka till första sidan.
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
-  //Metod som lägger til kurser med hjälp av min ScheduleService.
+  clearChoices(): void {
+    this.filterValue = '';
+    this.selectedSubject = '';
+    this.applyFilters();
+  }
+
   addCourse(course: Course) {
     console.log("Lägger till kurs:", course);
     this.scheduleService.addCourse(course);
   }
-
 }

635368619717251891d77276f5b83db4af461a85
Frida Jansson <frja2400@student.miun.se>
2025-05-19 14:06:58 +0200

    Kodar grundinställningar för body och main. Designar min header med layout och färgval. Croppar min headerbild.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink, RouterLinkActive],  //Importerar länkfunktioner till min nav-komponent.
+  imports: [RouterLink, RouterLinkActive, MatIconModule],  //Importerar länkfunktioner till min nav-komponent.
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
